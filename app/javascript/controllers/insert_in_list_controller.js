de6148b4e0df2d4b714f6c7fdabab9b3324c41a5
Christian Bongard <christian.bongard90@gmail.com>
2022-10-22 11:08:01 +0400

    Added the option to choose comment positioning

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="insert-in-list"
 export default class extends Controller {
   static targets = ["items", "form"]
+  static values = { position: String }
 
   connect() {
 
@@ -19,7 +20,7 @@ export default class extends Controller {
       .then(response => response.json())
       .then((data) => {
         if (data.inserted_item) {
-          this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+          this.itemsTarget.insertAdjacentHTML(this.positionValue, data.inserted_item)
         }
         this.formTarget.outerHTML = data.form
       })
