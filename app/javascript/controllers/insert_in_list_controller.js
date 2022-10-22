77f2f8700a7b34ee3b6ae2a93a55760a3c245eb5
Christian Bongard <christian.bongard90@gmail.com>
2022-10-22 10:46:39 +0400

    Added the send function for submit event

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -2,6 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="insert-in-list"
 export default class extends Controller {
+  static targets = ["items", "form"]
+
   connect() {
+
+  }
+
+  send(event) {
+    event.preventDefault()
+
+    console.log("TODO: send request in AJAX")
   }
 }
